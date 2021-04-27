2df7f661e47c7ffd1df058671be21c96e0d6267d
AfonSir <afonsir17@gmail.com>
2021-04-27 16:45:46 -0300

    feat: send and receiving events from posts service

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const { randomBytes } = require('crypto');
 
 const app = express();
@@ -13,7 +14,7 @@ app.get('/posts', (request, response) => {
   return response.send(posts);
 });
 
-app.post('/posts', (request, response) => {
+app.post('/posts', async (request, response) => {
   const id = randomBytes(4).toString('hex');
   const { title } = request.body;
 
@@ -21,7 +22,18 @@ app.post('/posts', (request, response) => {
 
   posts[id] = newPost;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: newPost
+  });
+
   return response.status(201).send(newPost);
+});
+
+app.post('/events', (request, response) => {
+  console.log('Received Event:', request.body.type);
+
+  return response.send({});
 });
 
 app.listen(4000, () => {
